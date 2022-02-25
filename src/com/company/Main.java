87669cf8651d8e83770a7dad2691be30dae2c797
norskemarcus <marc791j@stud.kea.dk>
2022-02-25 13:24:24 +0100

    added askAboutAge

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,19 +3,26 @@ import java.util.Scanner;
 
 public class Main {
 
+    Scanner input = new Scanner(System.in);
+
     public void greet(){
-        Scanner input = new Scanner(System.in);
+
         System.out.println("Tast ind dit navn: ");
         String navnBruger=  input.nextLine();
         System.out.println("Hej " + navnBruger);
     }
 
+    public void askAboutAge(){
+        System.out.println("Hvor gammel er du? ");
+        int alderBruger = input.nextInt();
+        System.out.println(alderBruger);
+    }
 
     public static void main(String[] args) {
-	// Prøveprojekt for at arbejde med git og github
         System.out.println("It works");
 
         Main obj = new Main();
         obj.greet();
+        obj.askAboutAge();
     }
 }
