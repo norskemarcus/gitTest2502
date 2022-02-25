d74a970471b3f4361ae94af7ca31f0a37c752714
norskemarcus <marc791j@stud.kea.dk>
2022-02-25 12:59:59 +0100

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Prøveprojekt for at arbejde med git og github
+        System.out.println("Nu virker koden");
+    }
+}
