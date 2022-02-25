d040d5c2fa4a1141589f08e9ac1a37050700ac86
norskemarcus <marc791j@stud.kea.dk>
2022-02-25 13:14:42 +0100

    added greet

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,21 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
+
+    public void greet(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Tast ind dit navn: ");
+        String navnBruger=  input.nextLine();
+        System.out.println("Hej " + navnBruger);
+    }
+
 
     public static void main(String[] args) {
 	// Prøveprojekt for at arbejde med git og github
         System.out.println("It works");
+
+        Main obj = new Main();
+        obj.greet();
     }
 }
